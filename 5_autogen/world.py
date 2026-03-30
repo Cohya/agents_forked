bc66b11a47a93d3c5895c6e864c9f1c5141b5dc9
YC <YanivAvoda@gmail.com>
2026-03-30 20:12:55 +0300

    changes

--- 5_autogen/world.py
+++ 5_autogen/world.py
@@ -5,7 +5,13 @@ from autogen_ext.runtimes.grpc import GrpcWorkerAgentRuntime
 from autogen_core import AgentId
 import messages
 import asyncio
+import os
+os.environ["PYTHONIOENCODING"] = "utf-8"
 
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 HOW_MANY_AGENTS = 20
 
 async def create_and_message(worker, creator_id, i: int):
@@ -24,7 +30,7 @@ async def main():
     result = await Creator.register(worker, "Creator", lambda: Creator("Creator"))
     creator_id = AgentId("Creator", "default")
     coroutines = [create_and_message(worker, creator_id, i) for i in range(1, HOW_MANY_AGENTS+1)]
-    await asyncio.gather(*coroutines)
+    await asyncio.gather(*coroutines) # run all the coroutines concurrently, which means we're creating and messaging all agents at the same time instead of waiting for each one to finish before starting the next one
     try:
         await worker.stop()
         await host.stop()
